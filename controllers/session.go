e48bc19cfa27a0524a734edfff6965efdd6e14f1
37203069@qq.com <37203069@qq.com>
2022-06-22 23:00:48 +0800

    实现登录页面

--- controllers/session.go
+++ controllers/session.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/json"
+	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
 	"iHome/models"
@@ -10,26 +12,92 @@ type SessionController struct {
 	beego.Controller
 }
 
-func (this *SessionController) RetData(resp map[string]interface{}) {
+type Name struct {
+	Name string `json:"name"`
+}
+
+type SessionResp struct {
+	Errno  string      `json:"errno"`
+	Errmsg string      `json:"errmsg"`
+	Data   interface{} `json:"data"`
+}
+
+func (this *SessionController) RetData(resp interface{}) {
 	this.Data["json"] = resp
 	this.ServeJSON()
 }
 func (c *SessionController) GetSessionData() {
+	resp := SessionResp{Errno: models.RECODE_OK, Errmsg: models.RecodeText(models.RECODE_OK)}
+	defer c.RetData(&resp)
+
+	name := c.GetSession("name")
+	if name == nil {
+		resp.Errno = models.RECODE_SESSIONERR
+		resp.Errno = models.RecodeText(resp.Errno)
+		return
+	}
+
+	NameData := Name{Name: name.(string)}
+	resp.Data = NameData
+
+	return
+
+}
+
+func (c *SessionController) DeleteSessionData() {
+	resp := SessionResp{Errno: models.RECODE_OK, Errmsg: models.RecodeText(models.RECODE_OK)}
+	defer c.RetData(&resp)
+
+	err := c.DelSession("name")
+	if err != nil {
+		resp.Errno = models.RECODE_SERVERERR
+		resp.Errmsg = models.RecodeText(resp.Errno)
+	}
+
+}
+
+func (c *SessionController) Login() {
+	//1. 获取用户信息
 	resp := make(map[string]interface{})
 	defer c.RetData(resp)
-	user := models.User{}
-	//user.Name = "test1"
-	//resp["errno"] = 0
-	//resp["errmsg"] = "OK"
-	//resp["data"] = user
-	resp["errno"] = models.RECODE_DBERR
-	resp["errmsg"] = models.RecodeText(models.RECODE_DBERR)
-	logs.Info("connect success")
-	name := c.GetSession("name")
-	if name != nil {
-		user.Name = name.(string)
-		resp["errno"] = models.RECODE_OK
-		resp["errmsg"] = models.RecodeText(models.RECODE_OK)
-		resp["data"] = user
+	//获取前端输入的jsonshju
+	json.Unmarshal(c.Ctx.Input.RequestBody, &resp)
+	logs.Info("==============name==========", resp["mobile"], "===============password==========", resp["password"])
+
+	//2. 判断是否合法
+	if resp["mobile"] == nil || resp["password"] == nil {
+		resp["errno"] = models.RECODE_DATAERR
+		resp["errmsg"] = models.RecodeText(models.RECODE_DATAERR)
+		logs.Info("==============name==========合法验证")
+		return
 	}
+	//3. 数据库匹配
+	o := orm.NewOrm()
+	user := models.User{Name: resp["mobile"].(string)}
+	qs := o.QueryTable("user")
+	_, err := qs.Filter("mobile", "111").All(&user)
+
+	if err != nil {
+		resp["errno"] = models.RECODE_DATAERR
+		resp["errmsg"] = models.RecodeText(models.RECODE_DATAERR)
+		logs.Info("==============name==========数据库匹配")
+
+		return
+	}
+	if user.Password_hash != resp["password"] {
+		resp["errno"] = models.RECODE_DATAERR
+		resp["errmsg"] = models.RecodeText(models.RECODE_DATAERR)
+		logs.Info("==============name==========密码匹配")
+
+		return
+	}
+	//4. 添加session
+	c.SetSession("name", resp["mobile"])
+	c.SetSession("mobile", resp["mobile"])
+	c.SetSession("user_id", user.Id)
+	//5. 返回json数据给前端
+
+	resp["errno"] = models.RECODE_OK
+	resp["errmsg"] = models.RecodeText(models.RECODE_OK)
+
 }
--- routers/router.go
+++ routers/router.go
@@ -9,6 +9,8 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/api/v1.0/areas", &controllers.AreaController{}, "get:GetArea")
 	beego.Router("/api/v1.0/houses/index", &controllers.HouseIndexController{}, "get:GetHouseIndex")
-	beego.Router("/api/v1.0/session", &controllers.SessionController{}, "get:GetSessionData")
+	beego.Router("/api/v1.0/session", &controllers.SessionController{}, "get:GetSessionData;delete:DeleteSessionData")
 	beego.Router("/api/v1.0/users", &controllers.UserController{}, "post:Reg")
+	beego.Router("/api/v1.0/sessions", &controllers.SessionController{}, "post:Login")
+
 }
